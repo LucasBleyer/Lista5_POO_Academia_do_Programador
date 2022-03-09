02d4b5dd78ea4c79a51eb57ecd83504a9da43d58
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-08 21:57:44 -0300

    ex7 refeito

---
+++ VolumeLata.ConsoleApp/Lata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeLata.ConsoleApp
+{
+    internal class Lata
+    {
+        public double raio;
+        public double altura;
+
+        public double CalcularVolume()
+        {
+            double volume = 3.14 * Math.Pow(raio, 2) * altura;
+            return Math.Round(volume, 2);
+        }
+    }
+}
