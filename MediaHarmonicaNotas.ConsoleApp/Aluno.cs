86ab37ef04cb1f0a49080728fe21e80b401f0e82
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-08 21:59:35 -0300

    ex8 refeito

---
+++ MediaHarmonicaNotas.ConsoleApp/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaHarmonicaNotas.ConsoleApp
+{
+    internal class Aluno
+    {
+        public double nota1;
+        public double nota2;
+        public double nota3;
+        public double nota4;
+
+        public double CalcularMediaHarmonica()
+        {
+            double media_harmonica = 4 / ((1 / nota1) + (1 / nota2) + (1 / nota3) + (1 / nota4));
+            return Math.Round(media_harmonica, 2);
+        }
+    }
+}
---
+++ MediaHarmonicaNotas.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MediaHarmonicaNotas.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            aluno.nota1 = 9;
+            aluno.nota2 = 10;
+            aluno.nota3 = 7;
+            aluno.nota4 = 4;
+
+            Console.WriteLine("Media harmônica das notas: " + aluno.CalcularMediaHarmonica());
+        }
+    }
+}
