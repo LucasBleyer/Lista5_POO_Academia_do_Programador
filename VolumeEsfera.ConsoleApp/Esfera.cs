2dcbc6e146c04b57bf7cb6a6641b9023224c57de
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-08 21:46:39 -0300

    ex4 refeito

---
+++ VolumeEsfera.ConsoleApp/Esfera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VolumeEsfera.ConsoleApp
+{
+    internal class Esfera
+    {
+        public double raio;
+
+        public double CalcularVolume()
+        {
+            double volume = 1.33 * Math.Pow(raio, 3) * 3.14;
+            return Math.Round(volume, 2);
+        }
+    }
+}
---
+++ VolumeEsfera.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VolumeEsfera.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Esfera esfera = new Esfera();
+            esfera.raio = 5;
+
+            Console.WriteLine("Volume: " + esfera.CalcularVolume());
+
+        }
+    }
+}
