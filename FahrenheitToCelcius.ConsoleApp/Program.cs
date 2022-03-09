c85adbe0dfdeca9f8f670cb2858f9df3b74541b1
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-08 21:37:26 -0300

    ex2 refeito

---
+++ FahrenheitToCelcius.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FahrenheitToCelcius.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperatura temp1 = new Temperatura();
+            temp1.fahrenheit = 48;
+
+            Console.WriteLine("Celsius: " + temp1.ConverterFahrenheitToCelcius());
+        }
+    }
+}
---
+++ FahrenheitToCelcius.ConsoleApp/Temperatura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FahrenheitToCelcius.ConsoleApp
+{
+    internal class Temperatura
+    {
+        public double fahrenheit;
+        public double ConverterFahrenheitToCelcius()
+        {
+            double celcius = fahrenheit - 32 / 1.8;
+            return Math.Round(celcius, 2);
+        }
+    }
+}
