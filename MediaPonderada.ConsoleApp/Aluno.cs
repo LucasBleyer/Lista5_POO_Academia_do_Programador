f81e692a9e49fd9fde0dd922b8ca76a5fcec742c
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-08 22:01:07 -0300

    ex10 refeito

---
+++ MediaPonderada.ConsoleApp/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaPonderada.ConsoleApp
+{
+    internal class Aluno
+    {
+        public double nota1;
+        public double nota2;
+
+        public double CalcularMediaPonderada()
+        {
+            double media_ponderada = (nota1 + nota2) / 2;
+            return Math.Round(media_ponderada, 2);
+        }
+    }
+}
---
+++ MediaPonderada.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MediaPonderada.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            aluno.nota1 = 9;
+            aluno.nota2 = 7;
+
+            Console.WriteLine("Media Ponderada: " + aluno.CalcularMediaPonderada());
+        }
+    }
+}
