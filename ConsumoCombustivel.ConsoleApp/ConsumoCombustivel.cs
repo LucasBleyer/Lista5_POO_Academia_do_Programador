ee7ab08609f105db478a63f33ad54d1aa49b0313
Lucas Bleyer <lucasbleyer19@gmail.com>
2022-03-08 21:42:51 -0300

    ex4 refeito

---
+++ ConsumoCombustivel.ConsoleApp/ConsumoCombustivel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumoCombustivel.ConsoleApp
+{
+    internal class ConsumoCombustivel
+    {
+        public double km_inicial;
+        public double km_final;
+        public double litros_inicial;
+        public double litros_final;
+
+        public double CalcularConsumo()
+        {
+            double consumo = (km_final - km_inicial) / (litros_inicial - litros_final);
+            return Math.Round(consumo, 2);
+        }
+    }
+}
---
+++ ConsumoCombustivel.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsumoCombustivel.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsumoCombustivel carro1 = new ConsumoCombustivel();
+            //variação de quilometros = 400km
+            carro1.km_inicial = 20;
+            carro1.km_final = 420;
+
+            //variação de litros = 34L
+            carro1.litros_inicial = 36;
+            carro1.litros_final = 2;
+
+            Console.WriteLine("Consumo: " + carro1.CalcularConsumo() + "Km/L");
+
+        }
+    }
+}
